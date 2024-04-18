a61cdbe59729a3530ace3853a864b137c97d97b9
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-18 12:09:50 +0300

    Some changes

---
+++ P03_Controllers/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using P03_Controllers.Models;
+using System.Diagnostics;
+
+namespace P03_Controllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ P03_Controllers/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P03_Controllers.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Action metotları
+
+        public string GetAll()
+        {
+            return "--- Tüm Öğrenciler ---";
+        }
+
+        public string GetOneStudent(string name)
+        {
+            return $"Bilgisini istemiş olduğunuz öğrenci adı : {name}";
+        }
+
+        // Student/Ortalama?not1=45&not2=55&not3=40
+        public string Ortalama(int not1,int not2,int not3)
+        {
+            int sonuc;
+
+            sonuc = (not1 + not2 +not3);
+
+            return $"Öğrenci not toplamı : {sonuc.ToString()}";
+        }
+    }
+}
