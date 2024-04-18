d9941dae2052845c1498c253aec04c0bba09ef70
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-18 11:13:15 +0300

    Some changes

---
+++ P01_MVCBasics/Controllers/HelloController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace P01_MVCBasics.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Hello MVC World");
+        }
+    }
+}
