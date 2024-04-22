98d1f292c5f78a0a9b4830497a26018007c7230a
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-22 11:26:19 +0300

    Some changes

---
+++ P06_RazorViewEngine/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P06_RazorViewEngine.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
