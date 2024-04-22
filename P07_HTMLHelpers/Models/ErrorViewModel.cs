a282e22271c25dae493612b205dbc37fe12222db
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-22 14:45:22 +0300

    Some changes

---
+++ P07_HTMLHelpers/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P07_HTMLHelpers.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ P07_HTMLHelpers/Models/Student.cs
@@ -0,0 +1,23 @@
+﻿namespace P07_HTMLHelpers.Models
+{
+    public enum Gender
+    {
+        Seçiniz,
+        Male,
+        Female,
+        Male2,
+        Female2
+    }
+
+
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string IlkAd { get; set; }
+        public string LastName { get; set; }
+        public string ClassName { get; set; }
+        public bool IsActive { get; set; }
+        public Gender StuGender { get; set; }
+
+    }
+}
