c2c3855900c8c5ea365779c87a9290cc921c2112
Arı Bilgi <nomail@aribilgi.com.tr>
2024-04-24 10:09:01 +0300

    Some changes

---
+++ P08_MVCSample/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace P08_MVCSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
